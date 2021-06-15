c3019f1217403f7977cf3779c7ea9963b02e3ac0
Jasmine Ryon <jryon12@priorypride.net>
2021-06-14 20:05:39 -0700

    commit tree fe3402638421c756326a129dfac64221154d47f2

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+    validates :name, presence: true, uniqueness: true
+    validates :phone_number, length: { minimum: 10, maximum: 10 }
+    # or validates :phone_number, length: { in: 10..10 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :is_clickbaity
+end
+
+    def is_clickbaity
+        unless [/Won't Believe/i,/Secret/i,/Guess/i,/Top [0-9]+/i].any? {|i| self.title=~ i }
+            errors.add(:title, "not clickbait-y")
+    end
 end
